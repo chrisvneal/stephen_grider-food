52ef7256f3d1a8d3f3d50b7cf974d4217973a963
Chris Neal <chrisvneal@gmail.com>
2021-06-06 18:06:15 -1000

    Add error handling message to catch block of try statement

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,33 +4,47 @@ import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-  // create state for the term being searched
+  // const num = 0
+  console.log("Hi there");
+  // set state
   const [term, setTerm] = useState("");
-
-  // create state to store results from the search
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   // function to use axios to retrieve results and store in state via state variable's setter function
-
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
+      // capture response frpm yelp api using parameters
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: "hawaii",
         },
       });
 
+      // once the response is received, store them in the results piece of state. The data is always in 'response.data' and we want the business object of that data
+
       setResults(response.data.businesses);
     } catch (err) {
-      // console.log(err);
+      console.log(err);
+
+      setErrorMessage("Something went wrong!");
     }
   };
 
+  // Call searchAPI when component is first rendered... bAD CODE!
+
+  // searchApi("pasta");
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
