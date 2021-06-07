0a214425628322d6bb90d4100c3a3817987bc691
Chris Neal <chrisvneal@gmail.com>
2021-06-06 18:06:15 -1000

    Add error handling message to catch block of try statement

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,16 +4,15 @@ import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-  // create state for the term being searched
+  // set state
   const [term, setTerm] = useState("");
-
-  // create state to store results from the search
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   // function to use axios to retrieve results and store in state via state variable's setter function
-
   const searchApi = async () => {
     try {
+      // capture response frpm yelp api using parameters
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
@@ -22,15 +21,20 @@ const SearchScreen = () => {
         },
       });
 
+      // once the response is received, store them in the results piece of state. The data is always in 'response.data' and we want the business object of that data
+
       setResults(response.data.businesses);
     } catch (err) {
-      // console.log(err);
+      console.log(err);
+
+      setErrorMessage("Something went wrong!");
     }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
