0acae6f354b00996e05c1085d8b15d69a568dbae
Chris Neal <chrisvneal@gmail.com>
2021-06-13 14:58:59 -1000

    Create separate file for SearchScreen API logic

---
+++ src/hooks/useResults.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // function to use axios to retrieve results and store in state via state variable's setter function
+
+  const searchApi = async (searchTerm) => {
+    try {
+      // capture response frpm yelp api using parameters
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "hawaii",
+        },
+      });
+
+      // once the response is received, store them in the results piece of state. The data is always in 'response.data' and we want the business object of that data
+
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err);
+
+      setErrorMessage("Something went wrong!");
+    }
+  };
+
+  // Call searchAPI when component is first rendered... bAD CODE!
+
+  // searchApi("pasta");
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+};
